5eef6d85e81d4fd9f95159f603d57cfbad32a2da
samyyc <s1myyc@outlook.com>
2025-10-11 19:59:34 +0800

    Add vfunc count, Optimize pattern scan

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,8 @@ pub use flags::*;
 
 #[cfg(test)]
 mod tests {
+    use std::time::Instant;
+
     use anyhow::Result;
 
     use super::*;
@@ -21,19 +23,27 @@ mod tests {
 
         s2binlib.load_binary("server");
 
+        // println!("vtable count {}", s2binlib.get_vtable_vfunc_count("engine2", "CServerSideClient")?);
+
+
         // s2binlib.load_binary("tier0");
-        println!("1");
-        println!("{:X}", s2binlib.find_vtable_va("server", "CTraceFilter")?);
+        // println!("1");
+        // println!("{:X}", s2binlib.find_vtable_va("server", "CTraceFilter")?);
+        
+        let start = Instant::now();
 
         let xref = s2binlib.pattern_scan_va("server", "4C 8D 35 ? ? ? ? 77")?;
-        println!("xref {:X}", xref);
-        println!("follow xref {:X}", s2binlib.follow_xref_va_to_va("server", xref)?);
 
-        let str = s2binlib.find_string_va("server", "Think_Update")?;
-        println!("str: {:X}", str);
-        s2binlib.dump_xrefs("server")?;
-        // println!("{:X}", s2binlib.find_export_va("tier0", "Plat_GetOSType")?);
-        println!("{:X}", s2binlib.find_vfunc_by_vtbname_va("server", "CCSPlayerController", 11)?);
+        let duration = start.elapsed();
+        println!("Time taken: {:?}", duration);
+        // println!("xref {:X}", xref);
+        // println!("follow xref {:X}", s2binlib.follow_xref_va_to_va("server", xref)?);
+
+        // let str = s2binlib.find_string_va("server", "Think_Update")?;
+        // println!("str: {:X}", str);
+        // s2binlib.dump_xrefs("server")?;
+        // // println!("{:X}", s2binlib.find_export_va("tier0", "Plat_GetOSType")?);
+        // println!("{:X}", s2binlib.find_vfunc_by_vtbname_va("server", "CCSPlayerController", 11)?);
 
         Ok(())
     }
