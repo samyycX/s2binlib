f05bc6e7f383e136ef75238eeba7b2f3cdb2ac6c
samyyc <s1myyc@outlook.com>
2025-10-30 00:45:35 +0800

    Add license

--- src/flags.rs
+++ src/flags.rs
@@ -1,3 +1,22 @@
+/************************************************************************************
+ *  S2BinLib - A static library that helps resolving memory from binary file
+ *  and map to absolute memory address, targeting source 2 game engine.
+ *  Copyright (C) 2025  samyyc
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ***********************************************************************************/
+ 
 use object::SectionFlags;
 
 pub fn is_readable(flags: SectionFlags) -> bool {
