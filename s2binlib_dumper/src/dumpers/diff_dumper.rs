4dd8668c17d46e184b5bb134ba506840aeee8b3f
samyyc <s1myyc@outlook.com>
2025-11-10 22:37:20 +0800

    License
    Former-commit-id: fb487d91f3e6e814c7af9793d4fc0e0b2cf76fb7

--- s2binlib_dumper/src/dumpers/diff_dumper.rs
+++ s2binlib_dumper/src/dumpers/diff_dumper.rs
@@ -1,3 +1,22 @@
+/************************************************************************************
+ *  S2BinLib - A static library that helps resolving memory from binary file
+ *  and map to absolute memory address, targeting source 2 game engine.
+ *  Copyright (C) 2025  samyyc
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ***********************************************************************************/
+
 use std::fs::{self, File};
 use std::io::Write;
 
@@ -42,7 +61,7 @@ pub fn dump_diff(s2binlib: &S2BinLib, dump_dir: &str, binary_name: &str, base_cl
   writeln!(file, "Children Count: {}", children)?;
   writeln!(file, "=============================================")?;
   for i in 0..diffs.len() {
-    writeln!(file, "{:<10} [DIFF {}]", format!("func_{}", i), diffs[i])?;
+    writeln!(file, "{:<10} [ DIFF {:<3} ]", format!("func_{}", i), diffs[i])?;
   }
   file.flush()?;
 
